3e34daa1f0042012698f326787230c666859a51e
Guoyu Ou <benogy@gmail.com>
2017-02-24 23:55:04 +0800

    remove hash_head and add qbuf APIs

--- hash.h
+++ hash.h
@@ -1,14 +1,6 @@
 #ifndef __HASH_H__
 #define __HASH_H__
 
-#include "list.h"
-#include <pthread.h>
-
-struct hash_head {
-    struct hlist_head head;
-    pthread_rwlock_t lock;
-};
-
 unsigned long simple_hash11(const char* str, unsigned int len);
 unsigned long simple_hash31(const char* str, unsigned int len);
 unsigned long djb_hash(const char* str, unsigned int len);
--- qbuf.h
+++ qbuf.h
@@ -1,6 +1,8 @@
 #ifndef __QBUF_H__
 #define __QBUF_H__
 
+#include <stdlib.h>
+
 struct qbuf {
     char* base;
     unsigned int len;
@@ -10,5 +12,29 @@ struct qbuf_ref {
     const char* base;
     unsigned int len;
 };
+
+static inline void qbuf_init(struct qbuf* q) {
+    q->base = NULL;
+    q->len = 0;
+}
+
+static inline void qbuf_destroy(struct qbuf* q) {
+    q->len = 0;
+    if (q->base) {
+        free(q->base);
+        q->base = NULL;
+    }
+}
+
+static inline int qbuf_resize(struct qbuf* q, unsigned int newsize) {
+    void* ptr = realloc(q->base, newsize);
+    if (ptr) {
+        q->base = ptr;
+        q->len = newsize;
+        return 0;
+    }
+
+    return -1;
+}
 
 #endif
