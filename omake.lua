38a729911338ad71197352ee774d271b7fcab808
Guoyu Ou <benogy@gmail.com>
2020-04-09 21:40:21 +0800

    Makefile changes

--- omake.lua
+++ omake.lua
@@ -1,6 +1,13 @@
 project = CreateProject()
 
-target = project:CreateLibrary("utils", STATIC | SHARED)
-target:AddSourceFiles("*.c")
+dep = project:CreateDependency()
+dep:AddFlags("-Wall", "-Werror", "-Wextra", "-fPIC")
+dep:AddSourceFiles("*.c")
+
+a = project:CreateStaticLibrary("utils_static")
+a:AddDependencies(dep)
+
+so = project:CreateSharedLibrary("utils_shared")
+so:AddDependencies(dep)
 
 return project
--- tests/omake.lua
+++ tests/omake.lua
@@ -1,23 +1,30 @@
 project = CreateProject()
 
+flags = "-Wall -Werror -Wextra -fPIC"
+
 test_rbtree = project:CreateBinary("test_rbtree")
+test_rbtree:AddFlags(flags)
 test_rbtree:AddSourceFiles("test_rbtree.c")
-test_rbtree:AddLibrary("..", "utils", STATIC)
+test_rbtree:AddStaticLibrary("..", "utils_static")
 
 test_str_util = project:CreateBinary("test_str_util")
+test_str_util:AddFlags(flags)
 test_str_util:AddSourceFiles("test_str_util.c")
-test_str_util:AddLibrary("..", "utils", STATIC)
+test_str_util:AddStaticLibrary("..", "utils_static")
 
 test_hash = project:CreateBinary("test_hash")
+test_hash:AddFlags(flags)
 test_hash:AddSourceFiles("test_hash.c")
-test_hash:AddLibrary("..", "utils", STATIC)
+test_hash:AddStaticLibrary("..", "utils_static")
 
 test_bitmap = project:CreateBinary("test_bitmap")
+test_bitmap:AddFlags(flags)
 test_bitmap:AddSourceFiles("test_bitmap.c")
-test_bitmap:AddLibrary("..", "utils", STATIC)
+test_bitmap:AddStaticLibrary("..", "utils_static")
 
 test_time_utils = project:CreateBinary("test_time_utils")
+test_time_utils:AddFlags(flags)
 test_time_utils:AddSourceFiles("test_time_utils.c")
-test_time_utils:AddLibrary("..", "utils", STATIC)
+test_time_utils:AddStaticLibrary("..", "utils_static")
 
 return project
