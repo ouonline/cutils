01b12cc3c5e2e58317ea88c01daa1cf1e372c322
Guoyu Ou <benogy@gmail.com>
2020-08-20 19:45:08 +0800

    remove native_aio.h

--- omake.lua
+++ omake.lua
@@ -1,13 +1,10 @@
 project = CreateProject()
 
 dep = project:CreateDependency()
-dep:AddFlags("-Wall", "-Werror", "-Wextra", "-fPIC")
-dep:AddSourceFiles("*.c")
+    :AddFlags("-Wall", "-Werror", "-Wextra", "-fPIC")
+    :AddSourceFiles("*.c")
 
-a = project:CreateStaticLibrary("utils_static")
-a:AddDependencies(dep)
-
-so = project:CreateSharedLibrary("utils_shared")
-so:AddDependencies(dep)
+project:CreateStaticLibrary("utils_static"):AddDependencies(dep)
+project:CreateSharedLibrary("utils_shared"):AddDependencies(dep)
 
 return project
--- tests/omake.lua
+++ tests/omake.lua
@@ -1,38 +1,33 @@
 project = CreateProject()
 
-dep1 = project:CreateDependency()
-dep1:AddFlags("-Wall", "-Werror", "-Wextra")
-dep1:AddSourceFiles("test_rbtree.c")
-dep1:AddStaticLibrary("..", "utils_static")
-test_rbtree = project:CreateBinary("test_rbtree")
-test_rbtree:AddDependencies(dep1)
+project:CreateBinary("test_rbtree"):AddDependencies(
+    project:CreateDependency()
+        :AddFlags("-Wall", "-Werror", "-Wextra")
+        :AddSourceFiles("test_rbtree.c")
+        :AddStaticLibrary("..", "utils_static"))
 
-dep2 = project:CreateDependency()
-dep2:AddFlags("-Wall", "-Werror", "-Wextra")
-dep2:AddSourceFiles("test_str_utils.c")
-dep2:AddStaticLibrary("..", "utils_static")
-test_str_utils = project:CreateBinary("test_str_utils")
-test_str_utils:AddDependencies(dep2)
+project:CreateBinary("test_str_utils"):AddDependencies(
+    project:CreateDependency()
+        :AddFlags("-Wall", "-Werror", "-Wextra")
+        :AddSourceFiles("test_str_utils.c")
+        :AddStaticLibrary("..", "utils_static"))
 
-dep3 = project:CreateDependency()
-dep3:AddFlags("-Wall", "-Werror", "-Wextra")
-dep3:AddSourceFiles("test_hash.c")
-dep3:AddStaticLibrary("..", "utils_static")
-test_hash = project:CreateBinary("test_hash")
-test_hash:AddDependencies(dep3)
+project:CreateBinary("test_hash"):AddDependencies(
+    project:CreateDependency()
+        :AddFlags("-Wall", "-Werror", "-Wextra")
+        :AddSourceFiles("test_hash.c")
+        :AddStaticLibrary("..", "utils_static"))
 
-dep4 = project:CreateDependency()
-dep4:AddFlags("-Wall", "-Werror", "-Wextra")
-dep4:AddSourceFiles("test_bitmap.c")
-dep4:AddStaticLibrary("..", "utils_static")
-test_bitmap = project:CreateBinary("test_bitmap")
-test_bitmap:AddDependencies(dep4)
+project:CreateBinary("test_bitmap"):AddDependencies(
+    project:CreateDependency()
+        :AddFlags("-Wall", "-Werror", "-Wextra")
+        :AddSourceFiles("test_bitmap.c")
+        :AddStaticLibrary("..", "utils_static"))
 
-dep5 = project:CreateDependency()
-dep5:AddFlags("-Wall", "-Werror", "-Wextra")
-dep5:AddSourceFiles("test_time_utils.c")
-dep5:AddStaticLibrary("..", "utils_static")
-test_time_utils = project:CreateBinary("test_time_utils")
-test_time_utils:AddDependencies(dep5)
+project:CreateBinary("test_time_utils"):AddDependencies(
+    project:CreateDependency()
+        :AddFlags("-Wall", "-Werror", "-Wextra")
+        :AddSourceFiles("test_time_utils.c")
+        :AddStaticLibrary("..", "utils_static"))
 
 return project
