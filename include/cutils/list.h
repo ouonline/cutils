8209d1dd57f1eb11b0810fd0e962456f54832135
GY <benogy@gmail.com>
2023-02-15 12:11:35 +0800

    list.h changes

--- include/cutils/list.h
+++ include/cutils/list.h
@@ -44,23 +44,23 @@ static inline void list_del(struct list_node* node) {
     list_init(node);
 }
 
-static inline struct list_node* list_first(struct list_node* head) {
+static inline struct list_node* list_first(const struct list_node* head) {
     return head->next;
 }
 
-static inline struct list_node* list_last(struct list_node* head) {
+static inline struct list_node* list_last(const struct list_node* head) {
     return head->prev;
 }
 
-static inline struct list_node* list_next(struct list_node* node) {
+static inline struct list_node* list_next(const struct list_node* node) {
     return node->next;
 }
 
-static inline struct list_node* list_prev(struct list_node* node) {
+static inline struct list_node* list_prev(const struct list_node* node) {
     return node->prev;
 }
 
-static inline int list_empty(struct list_node* head) {
+static inline int list_empty(const struct list_node* head) {
     return (head->next == head);
 }
 
@@ -87,36 +87,36 @@ static inline void list_move_next(struct list_node* src,
     }
 }
 
-#define list_for_each(p, head) \
-    for ((p) = (head)->next; (p) != (head); (p) = (p)->next)
+#define list_for_each(_cur_, _head_) \
+    for ((_cur_) = (_head_)->next; (_cur_) != (_head_); (_cur_) = (_cur_)->next)
 
-#define list_for_each_safe(p, n, head) \
-    for ((p) = (head)->next, (n) = (p)->next; \
-         (p) != (head); \
-         (p) = (n), (n) = (p)->next)
+#define list_for_each_safe(_cur_, _next_, _head_) \
+    for ((_cur_) = (_head_)->next, (_next_) = (_cur_)->next; \
+         (_cur_) != (_head_); \
+         (_cur_) = (_next_), (_next_) = (_cur_)->next)
 
-#define list_for_each_reverse(p, head) \
-    for ((p) = (head)->prev; (p) != (head); (p) = (p)->prev)
+#define list_for_each_reverse(_cur_, _head_) \
+    for ((_cur_) = (_head_)->prev; (_cur_) != (_head_); (_cur_) = (_cur_)->prev)
 
-#define list_for_each_reverse_safe(p, n, head) \
-    for ((p) = (head)->prev, (n) = (p)->prev; \
-         (p) != (head); \
-         (p) = (n), (n) = (p)->prev)
+#define list_for_each_reverse_safe(_cur_, _next_, _head_) \
+    for ((_cur_) = (_head_)->prev, (_next_) = (_cur_)->prev; \
+         (_cur_) != (_head_); \
+         (_cur_) = (_next_), (_next_) = (_cur_)->prev)
 
-#define list_for_each_from(p, pos, head) \
-    for ((p) = (pos); (p) != (head); (p) = (p)->next)
+#define list_for_each_from(_cur_, _pos_, _head_) \
+    for ((_cur_) = (_pos_); (_cur_) != (_head_); (_cur_) = (_cur_)->next)
 
-#define list_for_each_from_safe(p, n, pos, head) \
-    for ((p) = (pos), (n) = (p)->next; \
-         (p) != (head); \
-         (p) = (n), (n) = (p)->next)
+#define list_for_each_from_safe(_cur_, _next_, _pos_, _head_) \
+    for ((_cur_) = (_pos_), (_next_) = (_cur_)->next; \
+         (_cur_) != (_head_); \
+         (_cur_) = (_next_), (_next_) = (_cur_)->next)
 
-#define list_for_each_reverse_from(p, pos, head) \
-    for ((p) = (pos); (p) != (head); (p) = (p)->prev)
+#define list_for_each_reverse_from(_cur_, _pos_, _head_) \
+    for ((_cur_) = (_pos_); (_cur_) != (_head_); (_cur_) = (_cur_)->prev)
 
-#define list_for_each_reverse_from_safe(p, n, pos, head) \
-    for ((p) = (pos), (n) = (p)->prev; \
-         (p) != (head); \
-         (p) = (n), (n) = (p)->prev)
+#define list_for_each_reverse_from_safe(_cur_, _next_, _pos_, _head_) \
+    for ((_cur_) = (_pos_), (_next_) = (_cur_)->prev; \
+         (_cur_) != (_head_); \
+         (_cur_) = (_next_), (_next_) = (_cur_)->prev)
 
 #endif
